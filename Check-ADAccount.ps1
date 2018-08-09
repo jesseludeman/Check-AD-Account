b8e6285f0fdc8557b068b1fbf50239a998c3e0a9
Jesse Ludeman <jesse.ludeman@gmail.com>
2018-08-09 16:58:36 +1000

    Second commit

---
+++ Check-ADAccount.ps1
@@ -0,0 +1,29 @@
+﻿function Check-ADAccount
+{
+    $Name = Read-Host "Enter a username to check"
+    $User = Get-ADUser -Identity $Name -Properties LockedOut | Select LockedOut
+
+    while ($User.lockedout -eq $True)
+    {
+        Write-Host "$Name is currently locked out"
+
+        $UnlockAccount = Read-Host "Unlock the account? (y/n)"
+        if ($UnlockAccount -eq 'Y')
+        {
+            Unlock-ADAccount $Name
+            Write-Host "$Name now unlocked"
+            break
+        }
+        elseif ($UnlockAccount -eq 'N')
+        {
+            Write-Host "Ignoring $Name"
+            break
+        }
+    }
+
+    while ($User.lockedout -eq $False)
+    {
+        Write-Host "$Name is not currently locked out"
+        break
+    }
+}
